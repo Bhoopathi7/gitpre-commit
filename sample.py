58a3362f794d0deca48e36ce0d1b98297f4bd928
bhoopathi7 <bhoopathi.raj@blackstraw.ai>
2022-04-05 09:47:25 +0000

    sample

---
+++ sample.py
@@ -0,0 +1,10 @@
+import os
+import random
+
+
+def myLongFunction(var_a, var_b, var_c, var_d, var_e, var_f, var_g):
+    temp_var = var_b * var_c + var_d * var_e
+    another_temp_var = ["one", "two", "three"]
+
+    yet_another_variable = 5
+    return var_f + var_g
